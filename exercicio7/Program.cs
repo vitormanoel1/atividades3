4eba2f0d5c9039d3eb5797107a290603ee653329
vitor20022003 <vitorm4123@gmail.com>
2020-11-25 13:40:22 -0300

    exercicio concluido

--- exercicio7/Program.cs
+++ exercicio7/Program.cs
@@ -6,7 +6,26 @@ namespace exercicio7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+                Faça um algoritmo para ler 15 números e armazenar em um vetor. Após a leitura total dos
+                15 números, o algoritmo deve escrever esses 15 números lidos na ordem inversa da qual foi
+                declarado.
+            */
+
+            int[] numeros = new int[15];
+
+            for(int i = 0; i < numeros.Length; i++) 
+            {
+                Console.Write("Digite o numero: ");
+                numeros[i] = int.Parse(Console.ReadLine());  
+            }
+ 
+            Array.Reverse(numeros);
+            
+            for(int i = 0; i < numeros.Length; i++) 
+            {
+                Console.WriteLine(numeros[i]);
+            }
         }
     }
 }
