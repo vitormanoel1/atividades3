ebf00479cb7b95e4e1ac7fafbd32b8f4baef219f
vitor20022003 <vitorm4123@gmail.com>
2020-11-25 13:20:54 -0300

    exercicio concluido

--- exercicio6/Program.cs
+++ exercicio6/Program.cs
@@ -6,7 +6,32 @@ namespace exercicio6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+                Escreva um algoritmo que permita a leitura dos nomes de 10 pessoas e armazene os nomes
+                lidos em um vetor. Após isto, o algoritmo deve permitir a leitura de mais 1 nome qualquer de
+                pessoa (para efetuar uma busca) e depois escrever a mensagem ACHEI, se o nome estiver
+                entre os 10 nomes lidos anteriormente (guardados no vetor), ou NÃO ACHEI caso contrário.
+            */
+
+            string[] lista = new string[10];
+            for(int i = 1; i < lista.Length; i++) {
+                Console.Write("Digite o nome: ");
+                lista[i] = Console.ReadLine();
+            }
+            Console.WriteLine();
+
+            Console.Write("Digite nome para ser verificado na lista: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine();
+            
+            for(int i = 1; i < lista.Length; i++) {
+                if(nome == lista[i]) {
+                    Console.WriteLine($"achei {i}");
+                } else {
+                    Console.WriteLine($"nao achei {i}");
+                }
+            }
         }
     }
 }
